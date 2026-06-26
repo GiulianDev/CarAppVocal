bb258a0c4c0155d950b207df775ed02aacd8b51b
GiulianDev <39489811+Giulio-91@users.noreply.github.com>
2026-06-26 21:35:26 +0200

    hook useGarage

--- src/features/vehicleDetail/VehicleDetailView.tsx
+++ src/features/vehicleDetail/VehicleDetailView.tsx
@@ -1,61 +1,88 @@
 import { Button } from '../../shared/ui/Button';
 import { useCarContext } from '../../shared/context/CarContext';
 import type { Car } from '../../shared/types/car';
+import { useGarage } from '../../shared/hook/useGarage';
+import { Navigate, useParams } from 'react-router-dom';
 
 export function VehicleDetailView() {
 
+  const { id } = useParams<{ id: string }>();
 
+  const { getVehicle, isLoading } = useGarage();
 
+  // Se per qualche motivo strano non c'è l'ID, fai redirect
+  if (!id) {
+    return <Navigate to="/add-vehicle" replace />;
+  }
 
-  // Estraiamo l'azione dal context globale
-  const { deleteCar, setIsAdding } = useCarContext();
+  // Aspettiamo che l'hook finisca di leggere dal localStorage
+  if (isLoading) {
+    return <div className="p-8 text-center text-zinc-400">Caricamento veicolo...</div>;
+  }
+
+  // Ora recuperiamo l'auto (senza scatenare re-render)
+  const car = getVehicle(id);
 
   return (
     <div className="w-full max-w-md p-6 sm:p-8 bg-zinc-900/30 backdrop-blur-xl border border-zinc-800/80 rounded-2xl shadow-[0_20px_50px_rgba(0,0,0,0.5)]">
       
-      <div className="flex justify-between items-center mb-6">
-        <span className="bg-indigo-500/10 text-indigo-400 px-3 py-1 rounded-full text-xs font-semibold tracking-wide border border-indigo-500/20">
-          Veicolo Attivo
-        </span>
-        
-        <div className="flex items-center gap-2 w-auto">
-          {/* Modifica lo stato globale per andare in modalità inserimento */}
-          <Button variant="primary" onClick={() => setIsAdding(true)}>
-            <span className="mr-1.5 font-bold">+</span> Auto
-          </Button>
 
-          <Button 
-            variant="danger" 
-            onClick={() => {
-              if (confirm(`Vuoi davvero rimuovere la ${car.brand} dal garage?`)) {
-                deleteCar(car.id);
-              }
-            }}
-          >
-            Rimuovi
-          </Button>
+
+      {car ? (
+        <>
+          <div className="flex justify-between items-center mb-6">
+            <span className="bg-indigo-500/10 text-indigo-400 px-3 py-1 rounded-full text-xs font-semibold tracking-wide border border-indigo-500/20">
+              Veicolo Attivo
+            </span>
+            
+            <div className="flex items-center gap-2 w-auto">
+              {/* Modifica lo stato globale per andare in modalità inserimento */}
+              <Button variant="primary">
+                <span className="mr-1.5 font-bold">+</span> Auto
+              </Button>
+
+              {/* <Button 
+                variant="danger" 
+                onClick={() => {
+                  if (confirm(`Vuoi davvero rimuovere la ${car.brand} dal garage?`)) {
+                    deleteCar(car.id);
+                    }
+                    }}
+                    >
+                    Rimuovi
+                    </Button> */}
+            </div>
+          </div>
+          
+          <h1 className="text-3xl font-bold text-white mb-2 tracking-tight bg-gradient-to-r from-white via-zinc-200 to-zinc-400 bg-clip-text text-transparent">
+            {car?.brand}
+          </h1>
+          
+          <div className="inline-flex items-center bg-zinc-800/50 text-indigo-400 font-mono font-bold text-xl px-4 py-1.5 rounded-xl border border-zinc-700/60 tracking-widest my-2 shadow-inner">
+            <div className="w-1.5 h-4 bg-indigo-500 mr-2.5 rounded-sm animate-pulse" />
+            {car?.plate}
+          </div>
+
+          <div className="border-t border-zinc-800/60 my-6" />
+
+          <div className="bg-[#0b0b12]/60 border border-zinc-800/80 p-5 rounded-xl text-center backdrop-blur-sm">
+            <p className="font-semibold text-zinc-200 flex items-center justify-center gap-2">
+              <span>🎙️</span> Prossimo Step: Comando Vocale
+            </p>
+            <p className="text-xs text-zinc-500 mt-1.5 max-w-xs mx-auto leading-relaxed">
+              Qui integreremo l'interfaccia di registrazione per catturare i tuoi log vocali.
+            </p>
+          </div>
+        </>
+      ) : (
+        <>
+        <div>
+          id non trovato
         </div>
-      </div>
+        </>
+      )}
 
-      <h1 className="text-3xl font-bold text-white mb-2 tracking-tight bg-gradient-to-r from-white via-zinc-200 to-zinc-400 bg-clip-text text-transparent">
-        {car.brand}
-      </h1>
-      
-      <div className="inline-flex items-center bg-zinc-800/50 text-indigo-400 font-mono font-bold text-xl px-4 py-1.5 rounded-xl border border-zinc-700/60 tracking-widest my-2 shadow-inner">
-        <div className="w-1.5 h-4 bg-indigo-500 mr-2.5 rounded-sm animate-pulse" />
-        {car.plate}
-      </div>
 
-      <div className="border-t border-zinc-800/60 my-6" />
-
-      <div className="bg-[#0b0b12]/60 border border-zinc-800/80 p-5 rounded-xl text-center backdrop-blur-sm">
-        <p className="font-semibold text-zinc-200 flex items-center justify-center gap-2">
-          <span>🎙️</span> Prossimo Step: Comando Vocale
-        </p>
-        <p className="text-xs text-zinc-500 mt-1.5 max-w-xs mx-auto leading-relaxed">
-          Qui integreremo l'interfaccia di registrazione per catturare i tuoi log vocali.
-        </p>
-      </div>
 
     </div>
   );
---
+++ src/shared/hook/useGarage.ts
@@ -0,0 +1,88 @@
+// useGetVehicle.ts
+import { useEffect, useState } from "react";
+import type { Car } from "../types/car";
+
+export function useGarage() {
+
+  const [cars, setCars] = useState<Car[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const saved = localStorage.getItem('cars');
+    setCars(saved ? JSON.parse(saved) : []);
+    setIsLoading(false);
+
+    /* 💡 FUTURO CON FIREBASE & FIRESTORE:
+    if (user) {
+      setIsLoading(true);
+      // Ascolta Firestore in tempo reale per questo utente
+      const unsubscribe = onSnapshot(doc(db, "users", user.uid), (doc) => {
+        setCars(doc.data()?.cars || []);
+        setIsLoading(false);
+      });
+      return () => unsubscribe();
+    }
+      */
+  }, [/* user */]);
+
+  const getVehicle = (id: string | undefined) => {    
+    if (!id) return null;
+    return cars.find((c) => c.id === id) || null;
+  };
+
+  const addVehicle = (plate: string, brand: string, model: string) => {    
+    try {
+      const newCar: Car = {
+        id: crypto.randomUUID(),
+        plate: plate.toUpperCase(),
+        brand: `${brand} ${model}`,
+        addedAt: new Date().toISOString(),
+      };
+      const updated = [...cars, newCar];
+      setCars(updated);
+      localStorage.setItem('cars', JSON.stringify(updated));
+      console.log('Add vehicle success: ', newCar);
+      return newCar;
+    } finally {
+      setIsLoading(false); // Termina il processo (anche in caso di errore)
+    }
+
+    /* 💡 FUTURO CON FIRESTORE:
+    if (user) {
+      // Salva sul database cloud di Firebase
+      await updateDoc(doc(db, "users", user.uid), { cars: updatedCars });
+    }
+    */
+
+
+  };
+
+
+  const deleteCar = (id: string) => {
+    const updatedCars = cars.filter(car => car.id !== id);
+    setCars(updatedCars);
+    localStorage.setItem('carvoice_cars', JSON.stringify(updatedCars));
+    setIsLoading(false);
+
+    /* 💡 FUTURO CON FIRESTORE:
+    if (user) {
+      await updateDoc(doc(db, "users", user.uid), { cars: updatedCars });
+    }
+    */
+  };
+
+  const resetGarage = () => {
+    setCars([]); // Svuota lo stato di React
+    localStorage.removeItem('carvoice_cars'); // Elimina la chiave dal LocalStorage
+    setIsLoading(false);
+
+    /* 💡 FUTURO CON FIRESTORE:
+    if (user) {
+      await updateDoc(doc(db, "users", user.uid), { cars: [] });
+    }
+    */
+  };
+
+  // Restituisci lo stato VERO, non 'false' hardcodato
+  return { getVehicle, addVehicle, deleteCar, resetGarage, isLoading };
+}
