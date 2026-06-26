94c07a1d6f1db7ad57d279bc9f56310b64f08f18
GiulianDev <39489811+Giulio-91@users.noreply.github.com>
2026-06-26 20:03:31 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { CarProvider } from './shared/context/CarContext.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <CarProvider>
+    <BrowserRouter>
       <App />
-    </CarProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
